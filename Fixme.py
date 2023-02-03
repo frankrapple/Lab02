598cd60141ff2077b9971c83e334fc3f2e022a24
Ben <bsmith23@students.claremontmckenna.edu>
2023-02-03 10:08:38 -0800

    fixed fixme

--- Fixme.py
+++ Fixme.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
+
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
+
+    def is_even(n):
+        return n % 2 == 0
+
+    new_function = is_even
+
+    evens2 = list(filter(new_function, range(0, n+1)))
+    return evens2
